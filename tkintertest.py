edab24328fd5fb44fe9287638de81e3e86bce0cc
Patrick <patrickburns2557@gmail.com>
2023-01-17 13:59:36 -0600

    test user input

--- tkintertest.py
+++ tkintertest.py
@@ -4,10 +4,16 @@ from tkinter import ttk
 def yes():
     print("yes")
 
+
+
 root = Tk()
+userInput = StringVar()
 frame = ttk.Frame(root, padding=10)
 frame.grid()
 ttk.Label(frame, text="example text").grid(column=0, row=0)
 ttk.Button(frame, text="quit", command=root.destroy).grid(column=1, row=0)
 ttk.Button(frame, text="click", command=lambda: print("no")).grid(column=0, row=1)
+ttk.Entry(frame, textvariable=userInput).grid(column=0, row=2)
+ttk.Button(frame, text="Print Input", command=lambda: print(userInput.get())).grid(column=0, row=3)
 root.mainloop()
+print("Userinput: ", userInput.get())
